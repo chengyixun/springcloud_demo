51d7531e3b737aaa2ae04c5f83cc5dab6358cc80
amy <1501212682@qq.com>
2022-03-29 15:17:10 +0800

    add springCloud test

--- provider/src/main/java/com/example/provider/ProviderApplication.java
+++ provider/src/main/java/com/example/provider/ProviderApplication.java
@@ -3,11 +3,9 @@ package com.example.provider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient //开启服务注册与发现功能
-@EnableFeignClients //表示开始openFeign的扫描功能
 public class ProviderApplication {
 
 	public static void main(String[] args) {
